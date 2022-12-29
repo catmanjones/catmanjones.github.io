d325f8e8c1cb2976c7e06459f49acb7bc7909bd2
markflemingblog <121588784+markflemingblog@users.noreply.github.com>
2022-12-28 19:01:46 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+//Dark Mode
+https: //jsfiddle.net/mejwhasv/#
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+// FAQ Expand/Collapse
+
+function toggleElement(id) {
+  if (document.getElementById(id).style.display == 'none') {
+    document.getElementById(id).style.display = '';
+  } else {
+    document.getElementById(id).style.display = 'none';
+  }
+}
+
+// year
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("demo").innerHTML = year;
+
+// full page pop-up
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
