4531a56d6de191ccd00ce2fb2c02adc2571a75aa
markflemingblog <121588784+markflemingblog@users.noreply.github.com>
2023-07-11 17:05:34 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// FAQ Expand/Collapse
-
-function toggleElement(id) {
-  if (document.getElementById(id).style.display == 'none') {
-    document.getElementById(id).style.display = '';
-  } else {
-    document.getElementById(id).style.display = 'none';
-  }
-}
